730eaa5c68cf8d66d8a11e45ffac3b97d8dfa291
Oliver Lockwood (CBRE) <v-ollock@microsoft.com>
2020-10-23 10:11:49 -0700

    L4 prac v1 wsgi

--- bookapp.py
+++ bookapp.py
@@ -1,26 +1,60 @@
 import re
 
 from bookdb import BookDB
+import traceback
 
 DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+    return "Specific book with ID {}".format(book_id)
 
 
 def books():
     return "<h1>a list of books</h1>"
 
+def resolve_path(path):
+    funcs = {
+        '': books,
+        'book': book,
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
+    return func, args
 
 def application(environ, start_response):
-    status = "200 OK"
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     srv = make_server('localhost', 8080, application)
     srv.serve_forever()
+# python -u bookapp.py
